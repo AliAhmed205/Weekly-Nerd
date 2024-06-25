4385d5733804df827a88c50b3a95335b8013cee7
AliAhmed205 <Ali_lazuli@outlook.com>
2024-06-25 15:43:54 +0200

    Gif animated thumbnails

--- new-js.js
+++ new-js.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('.scroll-content');
+  const mobileNav = document.getElementById('mobile-navigation');
+  const speakersHeader = document.getElementById('speakers-header');
 
+  // Smooth scroll into view on focus
   links.forEach(link => {
     link.addEventListener('focus-visible', () => {
       link.scrollIntoView({
@@ -9,5 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
         inline: 'center'
       });
     });
+  });
+
+  // Add scroll event listener to window
+  window.addEventListener('scroll', () => {
+    // Get the top offset of #speakers-header
+    const speakersHeaderTop = speakersHeader.getBoundingClientRect().top + window.scrollY;
+
+    // Get the bottom offset of #mobile-navigation
+    const mobileNavBottom = mobileNav.getBoundingClientRect().bottom + window.scrollY;
+
+    // If the bottom of #mobile-navigation is above the top of #speakers-header, add the scroll class
+    if (mobileNavBottom >= speakersHeaderTop) {
+      mobileNav.classList.add('scroll');
+    } else {
+      mobileNav.classList.remove('scroll');
+    }
   });
 });
